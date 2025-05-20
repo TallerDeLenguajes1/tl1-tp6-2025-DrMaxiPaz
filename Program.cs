d781cb2f826023f05c5864b4f338b0f290505994
MaxiPaz <maxipaz0047@gmail.com>
2025-05-20 12:10:50 -0300

    ejercicio 1 listo

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("valor de a:" + a);
+Console.WriteLine("valor de b:" + b);
+
+Console.WriteLine("Ingrese un numero: ");
+string numero = Console.ReadLine();
+if (int.TryParse(numero, out int numeroEntero))
+{
+    Console.WriteLine("El numero es: " + numeroEntero);
+    if (numeroEntero > 0)
+    {
+        string invertido = "";
+        while (numeroEntero >= 1)
+        {
+            invertido += numeroEntero % 10;
+            numeroEntero /= 10;
+        }
+        Console.WriteLine("\n\tEl numero invertido es:" + invertido);
+    }
+    else
+    {
+        Console.WriteLine("\n\tEl numero es negativo");
+    }
+}
+else
+{
+    Console.WriteLine("No es un numero");
+}
+
