83fd567aaa5af7e280b00a6e2fe721fe07894c0a
MaxiPaz <maxipaz0047@gmail.com>
2025-05-20 12:57:32 -0300

    ejercicio 2 listo

--- Program.cs
+++ Program.cs
@@ -32,3 +32,39 @@ else
     Console.WriteLine("No es un numero");
 }
 
+
+Console.WriteLine("\n\t\tCalculadora");
+string opcion = "";
+
+while (opcion != "no")
+{
+    Console.WriteLine("\n\t1. Sumar\n\t2. Restar\n\t3. Multiplicar\n\t4. Dividir\n\tIngresa un operacion a realizar:");
+
+    string operacion = Console.ReadLine();
+    Console.WriteLine("Ingrese el primer numero: ");
+    float n1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el segundo numero: ");
+    float n2 = int.Parse(Console.ReadLine());
+    switch (operacion)
+    {
+        case "1":
+            Console.WriteLine("\n\tLa suma de los numeros es: " + (n1 + n2));
+            break;
+        case "2":
+            Console.WriteLine("\n\tLa resta de los numeros es: " + (n1 - n2));
+            break;
+        case "3":
+            Console.WriteLine("\n\tLa multiplicacion de los numeros es: " + (n1 * n2));
+            break;
+        case "4":
+            Console.WriteLine("\n\tLa division de los numeros es: " + (n1 / n2));
+            break;
+        default:
+            Console.WriteLine("\n\tOPcion invalida..");
+            break;
+    }
+    Console.WriteLine("\n\tDesea seguir calculando (si/no):");
+    opcion = Console.ReadLine();
+}
+
+
